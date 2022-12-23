42d48685258b74773bf089f978490aaac0f3c007
quietlycreeping <reachjenniferorlando@gmail.com>
2022-12-23 18:03:15 -0500

    working header links

--- app.js
+++ app.js
@@ -1,47 +1,36 @@
 //imports
-const port = 8080;
+const express = require('express')
+const app = express()
+const port = 8080
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+app.set('view engine', 'ejs')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+//===================================================
+//RENDER PAGES--------------------
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+//world pages
+app.get('/calendar', (req, res) => {
+  res.render('pages/World/calendar')
+})
+app.get('/city-map', (req, res) => {
+  res.render('pages/World/map')
+})
+app.get('/citizens', (req, res) => {
+  res.render('pages/World/people')
+})
+app.get('/the-team', (req, res) => {
+  res.render('pages/World/team')
+})
+//ooc pages
+app.get('/safety-policies', (req, res) => {
+  res.render('pages/OOC/consent')
+})
+app.get('/ruleset', (req, res) => {
+  res.render('pages/OOC/rules')
+})
+//===================================================
 
 //listen on port
 app.listen(port, () => console.info(`Listening on port ${port}`))

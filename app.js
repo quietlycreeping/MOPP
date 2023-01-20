87b56b37f52bb217d3ff54cff3494d3555e1e7b4
quietlycreeping <60121681+quietlycreeping@users.noreply.github.com>
2023-01-20 10:47:24 -0500

    improved footer

--- app.js
+++ app.js
@@ -5,40 +5,41 @@ const port = 8080
 
 app.set('view engine', 'ejs')
 
+//load folders
 app.use(express.static("public/stylesheets"));
 app.use(express.static("public/images"));
+app.use(express.static("public/scripts"));
 
 //===================================================
 //RENDER PAGES--------------------
 app.get('/', (req, res) => {
-    res.render('pages/index')
-})
+    res.render('pages/index')})
+    
 //world pages----
 app.get('/calendar', (req, res) => {
-  res.render('pages/World/calendar')
-})
+  res.render('pages/World/calendar')})
+
 app.get('/city-map', (req, res) => {
-  res.render('pages/World/map')
-})
+  res.render('pages/World/map')})
+
 app.get('/citizens', (req, res) => {
-  res.render('pages/World/people')
-})
+  res.render('pages/World/people')})
+
 app.get('/the-team', (req, res) => {
-  res.render('pages/World/team')
-})
+  res.render('pages/World/team')})
+
 //ooc pages-----
 app.get('/safety-policies', (req, res) => {
-  res.render('pages/OOC/consent')
-})
+  res.render('pages/OOC/consent')})
+
 app.get('/rules-moves', (req, res) => {
-  res.render('pages/OOC/rules-moves')
-})
+  res.render('pages/OOC/rules-moves')})
+
 app.get('/rules-labels', (req, res) => {
-  res.render('pages/OOC/rules-labels')
-})
-app.get('/about-us', (req, res) => {
-  res.render('pages/OOC/about')
-})
+  res.render('pages/OOC/rules-labels')})
+
+  app.get('/about-us', (req, res) => {
+  res.render('pages/OOC/about')})
 //===================================================
 
 //listen on port
